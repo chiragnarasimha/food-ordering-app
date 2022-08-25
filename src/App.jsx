f8062e17f2e2e0334ffacaa201a48914c46f93e2
chiragnarasimha <44315687+chiragnarasimha@users.noreply.github.com>
2022-08-25 18:07:04 +1000

    Added shadow to the top navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.scss";
 const App = (props) => {
   const render_content = () => {
     let content = [];
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 500; i++) {
       content.push(<div key={i}>Test Content</div>);
     }
     return content;
